a1d6e40288ace49187f4a17609b72ea9325ae9a6
Taylor Huffman <taylor@crucialartists.com>
2022-06-09 10:03:20 -0500

    commit tree 1bfc378c28c65726c9d4217c270ad9c707fb840f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+doc.css(".heading-financier").text.strip
+
+buttons = doc.css(".btn")
+buttons.each do |b|
+    puts b.text.strip
+end
+
+p doc.css(".heading-fanancier")[0].name
+p doc.css(".heading-fanancier")[0].attributes
